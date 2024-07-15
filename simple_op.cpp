76def2f4922d47c25d4653dd17f9ef0a9642cf0f
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-15 16:42:04 +0300

    Add project files.

---
+++ simple_op.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    long long b;
+    cin >> a;
+    cin >> b;
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << (a / b) << endl;
+    return 0;
+}
